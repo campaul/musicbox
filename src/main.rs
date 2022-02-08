6dcfefb8f02b921170a676a1cd1c9e382e1c1743
Cameron Paul <cpaul37@gmail.com>
2022-02-07 22:56:18 -0600

    Accept filename and track number as arguments

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ mod player;
 use std::io;
 use std::io::prelude::*;
 use std::fs::File;
+use std::env;
 
 use decoder::decode_song;
 use player::play_song;
@@ -13,8 +14,8 @@ const SEGMENT_MEMORY_OFFSET: usize = 188432;
 const SONG_HEADERS_ADDRESS: usize = 234816;
 const SEGMENT_HEADERS_ADDRESS: usize = SONG_HEADERS_ADDRESS - (37 * 7);
 
-fn load_rom(filename: &'static str) -> Result<Vec<u8>, std::io::Error> {
-    let mut f = File::open(filename)?;
+fn load_rom(filename: String) -> Result<Vec<u8>, std::io::Error> {
+    let mut f = File::open(filename.as_str())?;
     let mut rom = Vec::new();
 
     f.read_to_end(&mut rom)?;
@@ -22,10 +23,25 @@ fn load_rom(filename: &'static str) -> Result<Vec<u8>, std::io::Error> {
     Ok(rom)
 }
 
-fn main() -> io::Result<()> {
-    let rom = load_rom("smb3.nes")?;
+fn print_help() -> io::Result<()> {
+    println!("Usage: musicbox <filename> <track_number>");
+    Ok(())
+}
 
-    let song = decode_song(&rom, 0, SONG_HEADERS_ADDRESS, SEGMENT_HEADERS_ADDRESS, SEGMENT_MEMORY_OFFSET);
+fn main() -> io::Result<()> {
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() != 3 {
+        return print_help();
+    }
+
+    let filename = &args[1];
+    let track = &args[2].parse::<usize>().unwrap();
+
+    println!("Loading track {} from {}\n", track, filename);
+
+    let rom = load_rom(filename.clone())?;
+    let song = decode_song(&rom, *track, SONG_HEADERS_ADDRESS, SEGMENT_HEADERS_ADDRESS, SEGMENT_MEMORY_OFFSET);
 
     play_song(&song);
         
